cef985517020c86dd6f9acddfca8cdb0b0585e62
agmercier <72941971+agmercier@users.noreply.github.com>
2022-06-27 20:23:41 +0200

    Update MainMenu.cs
    add settings and level menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,11 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject settingsMenu;
+    public GameObject levelMenu;
     public void startGame()
     {
         SceneManager.LoadScene(1);
     }
 
+    public void OpenSettings()
+    {
+        settingsMenu.SetActive(true);
+    }
+    public void CloseSettings()
+    {
+        settingsMenu.SetActive(false);
+    }
+    public void OpenLevelMenu()
+    {
+        levelMenu.SetActive(true);
+    }
+    public void CloseLevelMenu()
+    {
+        levelMenu.SetActive(false);
+    }
     public void exitGame()
     {
         Debug.Log("Exiting...");
